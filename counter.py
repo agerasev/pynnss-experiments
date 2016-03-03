31ad628f369924048b9d0af98dddf99daa9bc575
Alexey Gerasev <alexey.gerasev@gmail.com>
2016-03-03 20:52:21 +0600

    rnn forward and backward test

--- counter.py
+++ counter.py
@@ -1,1 +1,42 @@
 #!/usr/bin/python3
+
+from pynn.element import Matrix, Sigmoid, Merger, Fork
+from pynn.path import Path
+from pynn.network import Network
+
+import numpy as np
+
+net = Network(1, 1)
+sin = 2
+shid = 4
+sout = 2
+
+net.nodes[0] = Matrix(sin, shid) # W_xh
+net.nodes[1] = Matrix(shid, shid) # W_hh
+net.nodes[2] = Merger(shid, 2)
+net.nodes[3] = Sigmoid(shid)
+net.nodes[4] = Fork(shid, 2)
+net.nodes[5] = Matrix(shid, sout) # W_hy
+
+net.paths.append(Path((-1, 0), ( 0, 0)))
+net.paths.append(Path(( 0, 0), ( 2, 0)))
+net.paths.append(Path(( 1, 0), ( 2, 1)))
+
+net.paths.append(Path(( 2, 0), ( 3, 0)))
+net.paths.append(Path(( 3, 0), ( 4, 0)))
+
+net.paths.append(Path(( 4, 1), ( 1, 0)))
+net.paths.append(Path(( 4, 0), ( 5, 0)))
+net.paths.append(Path(( 5, 0), (-1, 0)))
+
+net.update()
+
+mem = net.Memory()
+mem.pipes[net._fpath_link[(1, 0)]].data = np.zeros(shid) # put data in value loopback
+(mem, _) = net.feedforward(mem, [np.zeros(sin)])
+
+exp = net.Experience()
+exp.pipes[net._bpath_link[(1, 0)]].data = np.zeros(shid) # put data in error loopback
+net.backprop(exp, mem, [np.zeros(sout)])
+
+# Maybe detect loopbacks and put zero vectors there automatically?
--- odd-even.py
+++ odd-even.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from pynn.element import Product, Sigmoid
+from pynn.element import Matrix, Bias, Sigmoid
 from pynn.path import Path
 from pynn.network import Network
 
@@ -10,19 +10,17 @@ import numpy as np
 
 net = Network(1, 1)
 
-nin = 16
-nout = 4
+nin = 4
+nout = 2
 
-net.nodes[0] = Product(nin, nout)
-net.nodes[1] = Sigmoid(nout)
+net.nodes[0] = Matrix(nin, nout)
 
-net.paths.append(Path((-1, 0), (0, 0)))
-net.paths.append(Path((0, 0), (1, 0)))
-net.paths.append(Path((1, 0), (-1, 0)))
+net.paths.append(Path((-1, 0), ( 0, 0)))
+net.paths.append(Path(( 0, 0), (-1, 0)))
 
 net.update()
 
-batch_size = 0x40
+batch_size = 0x10
 batches_num = 0x100
 
 for k in range(0x10):
@@ -54,3 +52,16 @@ for k in range(0x10):
 		net.learn(exp, 1e-2/batch_size)
 
 	print(str(k) + ' cost: ' + str(cost/batch_size/batches_num))
+
+print(net.nodes[0].weight)
+
+test = False
+while test:
+	num = int(input('Enter number from 0 to ' + str(nin - 1) + ' (-1 to exit): '))
+	if num < 0:
+		break
+	lin = [0]*nin
+	lin[num] = 1
+	vins = [np.array(lin)]
+	(mem, vouts) = net.feedforward(net.Memory(), vins)
+	print('Result: ' + str(np.argmax(vouts[0])))
